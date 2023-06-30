e18d0e9135721b5759000aad4d220ec12b12caf5
yegorov <evgenyyegorov@gmail.com>
2023-06-30 14:19:00 +0400

    Format and "enable_dns_hostnames"

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
   tags = {
     Name = var.vpc_name
   }
--- variables.tf
+++ variables.tf
@@ -9,3 +9,10 @@ variable "vpc_name" {
   type        = string
   nullable    = false
 }
+
+variable "enable_dns_hostnames" {
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC"
+  type        = bool
+  default     = false
+  nullable    = false
+}
