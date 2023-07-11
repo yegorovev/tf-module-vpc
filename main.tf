d53ad5e05a9e99d30784a545fcf2196972ecac8c
yegorov <evgenyyegorov@gmail.com>
2023-07-11 18:45:00 +0400

    Fixed

--- main.tf
+++ main.tf
@@ -5,3 +5,20 @@ resource "aws_vpc" "vpc" {
     Name = var.vpc_name
   }
 }
+
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    protocol  = -1
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "vpc" {
   description = "AWS VPC"
   value       = aws_vpc.vpc
 }
+
+output "default_sg" {
+  description = "Default security group"
+  value       = aws_default_security_group.default_sg
+}
