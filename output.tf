e748fac24efaa21873ae46a6a122935662d819c7
yegorovev <91023323+yegorovev@users.noreply.github.com>
2023-07-20 18:21:18 +0400

    Fixed (#7)

--- output.tf
+++ output.tf
@@ -2,8 +2,3 @@ output "vpc" {
   description = "AWS VPC"
   value       = aws_vpc.vpc
 }
-
-output "default_sg" {
-  description = "Default security group"
-  value       = aws_default_security_group.default_sg
-}
